67172bcccc6dfbf3d2230067ace0a50f1e413390
AbimanyuGilar <gilarwaluyo@gmail.com>
2024-09-30 13:32:03 +0700

    Mengubah code agar tidak memakai function konversi

--- main.cpp
+++ main.cpp
@@ -1,29 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// Deklarasi function konversi
-string convert_from_c(double temp_input) {
-    double fahrenheit = temp_input * 9.0 / 5.0 + 32;
-    double reamur = temp_input * 4.0 / 5.0;
-
-    return "Hasil:\nFahrenheit = " + to_string(fahrenheit) + "\nReamur = " + to_string(reamur) + "\n";
-}
-
-string convert_from_f(double temp_input) {
-    double celsius = (temp_input - 32) * 5.0 / 9.0;
-    double reamur = celsius * 4.0 / 5.0;
-
-    return "Hasil:\ncelsius = " + to_string(celsius) + "\nReamur = " + to_string(reamur) + "\n";
-}
-
-string convert_from_r(double temp_input) {
-    double celsius = temp_input * 5.0 / 4.0;
-    double fahrenheit = celsius * 9.0 / 5.0 + 32;
-
-    return "Hasil:\ncelsius = " + to_string(celsius) + "\nfahrenheit = " + to_string(fahrenheit) + "\n";
-}
-
-//Function utama
 int main() {
     cout << "Masukkan tipe suhu (C/F/R)" << endl << ">>";
 
@@ -36,15 +13,27 @@ int main() {
     if (temp_type == "C" || temp_type == "c") {
         cout << "Masukkan nilai suhu dalam celsius" << endl << ">>";
         cin >> temp_input;
-        temp_output = convert_from_c(temp_input);
+
+        double fahrenheit = temp_input * 9.0 / 5.0 + 32;
+        double reamur = temp_input * 4.0 / 5.0;
+
+        temp_output = "Hasil:\nFahrenheit = " + to_string(fahrenheit) + "\nReamur = " + to_string(reamur) + "\n";
     } else if (temp_type == "F" || temp_type == "f") {
         cout << "Masukkan nilai suhu dalam fahrenheit" << endl << ">>";
         cin >> temp_input;
-        temp_output = convert_from_f(temp_input);
+
+        double celsius = (temp_input - 32) * 5.0 / 9.0;
+        double reamur = celsius * 4.0 / 5.0;
+
+        temp_output = "Hasil:\ncelsius = " + to_string(celsius) + "\nReamur = " + to_string(reamur) + "\n";
     } else if (temp_type == "R" || temp_type == "r") {
         cout << "Masukkan nilai suhu dalam reamur" << endl << ">>";
         cin >> temp_input;
-        temp_output = convert_from_r(temp_input);
+
+        double celsius = temp_input * 5.0 / 4.0;
+        double fahrenheit = celsius * 9.0 / 5.0 + 32;
+
+        temp_output = "Hasil:\ncelsius = " + to_string(celsius) + "\nfahrenheit = " + to_string(fahrenheit) + "\n";
     } else {
         cout << "Masukkan tipe suhu dengan benar!" << endl;
         main();
