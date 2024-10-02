18ccbea3e8fc15ca428a976f4f0ffb943a540069
AbimanyuGilar <gilarwaluyo@gmail.com>
2024-10-02 10:27:57 +0700

    memperbaiki main.cpp

--- main.cpp
+++ main.cpp
@@ -2,23 +2,24 @@
 using namespace std;
 
 int main() {
-    cout << "Masukkan besar suhu >> "; 
+    string output;
+    int conv;
     double n;
+    double celsius;
+    double fahrenheit;
+    double reamur;
+
+    cout << "Masukkan besar suhu >> "; 
     cin >> n; cout << endl;
 
+    while (true) {
     cout << "pilih metode convert:\n" << 
     "1. celsius ke fahrenheit dan reamur\n" <<
     "2. fahrenheit ke celsius dan reamur\n" <<
     "3. reamur ke celsius dan fahrenheit\n" <<
     ">> "; 
 
-    int conv;
     cin >> conv; cout << endl;
-
-    string output;
-    double celsius;
-    double fahrenheit;
-    double reamur;celsius * 9.0 / 5.0 + 32;
 
     switch (conv)
     {
@@ -41,7 +42,12 @@ int main() {
         cout << "Konversi dari Reamur ke Celsius dan ke Fahrenheit\nReamur = " << reamur << endl << "Celsius = " << celsius << endl << "Fahrenheit = " << fahrenheit << endl << endl;
         break;
     default:
+        cout << "***Masukkan metode convert dengan benar!***" << endl << endl;
+        continue;
         break;
+    }
+
+    break;
     }
 
     return 0;
