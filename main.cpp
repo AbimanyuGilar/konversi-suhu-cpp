ad751c6ec2903fe3be808580f3adf06b4c9b817b
AbimanyuGilar <gilarwaluyo@gmail.com>
2024-10-02 10:30:02 +0700

    merapikan main.cpp

--- main.cpp
+++ main.cpp
@@ -21,33 +21,32 @@ int main() {
 
     cin >> conv; cout << endl;
 
-    switch (conv)
-    {
-    case 1:
-        celsius = n;
-        fahrenheit = celsius * 9.0 / 5.0 + 32;
-        reamur = celsius * 4.0 / 5.0;
-        cout << "Konversi dari Celsius ke Fahrenheit dan ke Reamur\nCelsius = " << celsius << endl << "Fahrenheit = " << fahrenheit << endl << "Reamur = " << reamur << endl << endl;
-        break;
-    case 2:
-        fahrenheit = n;
-        celsius = (n - 32) * 5.0 / 9.0;
-        reamur = celsius * 4.0 / 5.0;
-        cout << "Konversi dari Fahrenheit ke Celsius dan ke Reamur\nFahrenheit = " << fahrenheit << endl << "Celsius = " << celsius << endl << "Reamur = " << reamur << endl << endl;
-        break;
-    case 3: 
-        reamur = n;
-        celsius = 5 * n / 4;
-        fahrenheit = celsius * 9.0 / 5.0 + 32;
-        cout << "Konversi dari Reamur ke Celsius dan ke Fahrenheit\nReamur = " << reamur << endl << "Celsius = " << celsius << endl << "Fahrenheit = " << fahrenheit << endl << endl;
-        break;
-    default:
-        cout << "***Masukkan metode convert dengan benar!***" << endl << endl;
-        continue;
-        break;
-    }
+    switch (conv) {
+        case 1:
+            celsius = n;
+            fahrenheit = celsius * 9.0 / 5.0 + 32;
+            reamur = celsius * 4.0 / 5.0;
+            cout << "Konversi dari Celsius ke Fahrenheit dan ke Reamur\nCelsius = " << celsius << endl << "Fahrenheit = " << fahrenheit << endl << "Reamur = " << reamur << endl << endl;
+            break;
+        case 2:
+            fahrenheit = n;
+            celsius = (n - 32) * 5.0 / 9.0;
+            reamur = celsius * 4.0 / 5.0;
+            cout << "Konversi dari Fahrenheit ke Celsius dan ke Reamur\nFahrenheit = " << fahrenheit << endl << "Celsius = " << celsius << endl << "Reamur = " << reamur << endl << endl;
+            break;
+        case 3: 
+            reamur = n;
+            celsius = 5 * n / 4;
+            fahrenheit = celsius * 9.0 / 5.0 + 32;
+            cout << "Konversi dari Reamur ke Celsius dan ke Fahrenheit\nReamur = " << reamur << endl << "Celsius = " << celsius << endl << "Fahrenheit = " << fahrenheit << endl << endl;
+            break;
+        default:
+            cout << "***Masukkan metode convert dengan benar!***" << endl << endl;
+            continue;
+            break;
+        }
 
-    break;
+        break;
     }
 
     return 0;
